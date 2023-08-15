c42f4b9a8e2ac0a79bc788734d7390e1d88fe553
leocklaus <leocklaus@gmail.com>
2023-08-15 17:38:38 -0300

    feat: check transaction status method created

--- src/main/java/com/lck/picpaysimplificado/domain/service/TransactionService.java
+++ src/main/java/com/lck/picpaysimplificado/domain/service/TransactionService.java
@@ -1,6 +1,8 @@
 package com.lck.picpaysimplificado.domain.service;
 
+import com.lck.picpaysimplificado.api.dto.TransactionAuthDTO;
 import com.lck.picpaysimplificado.api.dto.TransactionDTO;
+import com.lck.picpaysimplificado.domain.exception.TransactionNonAuthorizedException;
 import com.lck.picpaysimplificado.domain.exception.UserNotFoundException;
 import com.lck.picpaysimplificado.domain.model.Transaction;
 import com.lck.picpaysimplificado.domain.model.User;
@@ -8,7 +10,9 @@ import com.lck.picpaysimplificado.domain.repository.TransactionRepository;
 import com.lck.picpaysimplificado.domain.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class TransactionService {
@@ -27,8 +31,13 @@ public class TransactionService {
         Double transactionAmount = transactionDTO.getAmount();
 
         if(sender.canSendMoney() && sender.canWithdraw(transactionAmount)){
-            sender.withdraw(transactionAmount);
-            receiver.receiveMoney(transactionAmount);
+
+            if(transactionAuthorizationServiceReturnsSuccess()){
+                sender.withdraw(transactionAmount);
+                receiver.receiveMoney(transactionAmount);
+            } else {
+                throw new TransactionNonAuthorizedException();
+            }
         }
 
         transaction = transactionRepository.save(transaction);
@@ -40,6 +49,17 @@ public class TransactionService {
         var user = userRepository.findById(id)
                 .orElseThrow(()->  new UserNotFoundException(id));
         return user;
+    }
+
+    private boolean transactionAuthorizationServiceReturnsSuccess(){
+        final var URL = "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6";
+        final var SUCCESS_MESSAGE = "Autorizado";
+        var restTemplate = new RestTemplate();
+
+        ResponseEntity<TransactionAuthDTO> transactionAuthDTO =
+                restTemplate.getForEntity(URL, TransactionAuthDTO.class);
+
+        return transactionAuthDTO.getBody().getMessage().equals(SUCCESS_MESSAGE);
     }
 
 }
