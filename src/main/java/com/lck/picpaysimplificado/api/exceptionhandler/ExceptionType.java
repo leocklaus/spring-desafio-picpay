c5e92a8a89aacb48bc48066608b4fb8e08ef72dd
leocklaus <leocklaus@gmail.com>
2023-08-16 18:48:21 -0300

    feat: exception types enum created

---
+++ src/main/java/com/lck/picpaysimplificado/api/exceptionhandler/ExceptionType.java
@@ -0,0 +1,20 @@
+package com.lck.picpaysimplificado.api.exceptionhandler;
+
+public enum ExceptionType {
+
+    TRANSACTION_NON_AUTHORIZED("Transação não autorizada", "/non-authorized-transaction"),
+    USER_CANNOT_SEND_MONEY("Usuário não pode enviar dinheiro", "/user-cannot-send-money"),
+    USER_DOESNT_HAVE_ENOUGH_BALANCE("Saldo insuficiente", "/not-enought-balance"),
+    USER_NOT_FOUND("Id inválido", "/user-not-found"),
+    SENDER_IS_THE_RECEIVER("Mesmo usuário envia e recebe", "/sender-is-also-the-receiver");
+
+
+    private String title;
+    private String URI;
+
+    ExceptionType(String title, String URI){
+        this.title = title;
+        this.URI = URI;
+    }
+
+}
