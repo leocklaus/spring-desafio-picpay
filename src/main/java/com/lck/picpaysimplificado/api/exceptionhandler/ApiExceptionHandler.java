bb857710d9aad9edd3465265313ef2708dd95700
leocklaus <leocklaus@gmail.com>
2023-08-17 09:48:11 -0300

    feat: bean validation exception handler added

--- src/main/java/com/lck/picpaysimplificado/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/lck/picpaysimplificado/api/exceptionhandler/ApiExceptionHandler.java
@@ -5,12 +5,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -84,14 +89,81 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        var bindingResult = ex.getBindingResult();
+        String detail = "Um ou mais campos estão inválidos. Preencha o formulário novamente.";
+
+        List<ExceptionModel.Object> errors = new ArrayList<>();
+
+        errors = bindingResult.getFieldErrors().stream()
+                .map(error -> {
+                    return ExceptionModel.Object.builder()
+                            .name(error.getField())
+                            .userMessage(error.getDefaultMessage())
+                            .build();
+                }).toList();
+
+        var exceptionType = ExceptionType.INVALID_DATA;
+
+        ExceptionModel exception = exceptionBuilder((HttpStatus) status, exceptionType, detail)
+                .userMessage(detail)
+                .objects(errors)
+                .build();
+
+        return handleExceptionInternal(ex, exception, headers, status, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity<?> handleSystemException(Exception ex, WebRequest request){
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        var exceptionType = ExceptionType.SYSTEM_ERROR;
+
+        ExceptionModel exception = exceptionBuilder(status, exceptionType, GENERIC_ERROR_MESSAGE)
+                .userMessage(GENERIC_ERROR_MESSAGE)
+                .build();
+
+        ex.printStackTrace();
+
+
+        return handleExceptionInternal(ex, exception, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        var detail = String.format("O recurso '%s' que você tentou acessar é inexistente", ex.getRequestURL());
+        var exceptionType = ExceptionType.RESOURCE_NOT_FOUND;
+
+        ExceptionModel exception = exceptionBuilder((HttpStatus) status, exceptionType, detail)
+                .userMessage(detail)
+                .build();
+
+        return handleExceptionInternal(ex, exception, headers, status, request);
+    }
+
+    @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+
+        if(body == null){
+            body = ExceptionModel.builder()
+                    .status(statusCode.value())
+                    .type(ex.getMessage())
+                    .title(ex.getMessage())
+                    .detail(ex.getMessage())
+                    .timestamp(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS))
+                    .build();
+        }
+
         return super.handleExceptionInternal(ex, body, headers, statusCode, request);
     }
 
     private ExceptionModel.ExceptionModelBuilder exceptionBuilder(HttpStatus status, ExceptionType exceptionType, String detail){
         var exception = ExceptionModel.builder()
                 .status(status.value())
-                .timestamp(LocalDateTime.now())
+                .timestamp(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS))
                 .type(exceptionType.getURI())
                 .title(exceptionType.getTitle())
                 .detail(detail);
