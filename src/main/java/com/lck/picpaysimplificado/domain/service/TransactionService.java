19fc7235700c4f72cf9ae19ed14fdc4adc8cd227
leocklaus <leocklaus@gmail.com>
2023-08-17 10:16:24 -0300

    feat: get transaction and transaction by idd methods created

--- src/main/java/com/lck/picpaysimplificado/domain/service/TransactionService.java
+++ src/main/java/com/lck/picpaysimplificado/domain/service/TransactionService.java
@@ -3,6 +3,7 @@ package com.lck.picpaysimplificado.domain.service;
 import com.lck.picpaysimplificado.api.dto.TransactionAuthDTO;
 import com.lck.picpaysimplificado.api.dto.TransactionDTO;
 import com.lck.picpaysimplificado.domain.exception.TransactionNonAuthorizedException;
+import com.lck.picpaysimplificado.domain.exception.TransactionNotFoundException;
 import com.lck.picpaysimplificado.domain.exception.UserNotFoundException;
 import com.lck.picpaysimplificado.domain.model.Transaction;
 import com.lck.picpaysimplificado.domain.model.User;
@@ -14,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Service
 public class TransactionService {
 
@@ -22,6 +25,19 @@ public class TransactionService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<TransactionDTO> getAll() {
+        List<Transaction> transactions = transactionRepository.findAll();
+        return transactions.stream()
+                .map(TransactionDTO::new)
+                .toList();
+    }
+
+    public TransactionDTO getById(Long transactionId) {
+        var transaction = transactionRepository.findById(transactionId)
+                .orElseThrow(()-> new TransactionNotFoundException(transactionId));
+        return new TransactionDTO(transaction);
+    }
 
     @Transactional
     public TransactionDTO createTransaction(TransactionDTO transactionDTO){
@@ -61,5 +77,6 @@ public class TransactionService {
 
         return transactionAuthDTO.getBody().getMessage().equals(SUCCESS_MESSAGE);
     }
+
 
 }
